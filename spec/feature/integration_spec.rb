c8b9b7d93cd3a9d7d5c2aea2af61cbc5fbeec9b5
root <root@localhost>
2022-09-12 21:51:23 +0000

    added tests for author, price, and published date

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'j.k. rowling'
+    fill_in 'Price', with: 13.49
+    fill_in 'Published Date', with: 1997-6-26
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('j.k. rowling')
+    expect(page).to have_content(13.49)
+    expect(page).to have_content(1997-6-26)
   end
 end
